62750ccea745c06c1e1af162bbfa9b28a999bb98
KTregubov <31662934+KTregubov@users.noreply.github.com>
2018-11-18 23:21:33 -0500

    Finished Black Jack Assignment

--- BlackJack Port/assets/js/script.js
+++ BlackJack Port/assets/js/script.js
@@ -3,9 +3,9 @@ var NUM_SUITS = 4;
 var NUM_FACES = 13;
 var BLACK_JACK = 21;
 var MIN_BET = 5;
-var HIT = 1;
 var ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 // Player Variables
+var cardStack;
 var hash;
 var playerBalance;
 var currentBet;
@@ -21,14 +21,32 @@ var dealerHand;
 var dealerHandValueLowAce;
 var dealerHandValue;
 
+function restart() {
+    updateBalance();
+    document.getElementById("end").style.opacity = '0';
+    document.getElementById("nav").style.opacity = '0';
+    setTimeout(function () {
+        document.getElementById("end").style.display = "none";
+        document.getElementById("nav").style.display = "none";
+        document.getElementById("start").style.display = "flex";
+        setTimeout(function () {
+            document.getElementById("start").style.opacity = '1';
+        }, 200);
+    }, 500);
+}
+
 function startGame() {
     console.log("Started Game");
     playerBalance = 500;
-
-    // Show bet placement view
-    updateBalance();
     resetValues();
     transition(true, "start", "bet");
+}
+
+function continueGame() {
+    transition(true, "end", "bet");
+    setTimeout(function () {
+        resetValues();
+    }, 500);
 }
 
 function placeBet() {
@@ -36,63 +54,159 @@ function placeBet() {
     if (isBetValid) {
         playerBalance -= currentBet;
 
-        // Process 
-        var dealerCard = getCard();
-        dealerHand[0] = dealerCard;
-        if (isAce(dealerCard))
-            dealerNumAces++;
-        dealerHandValueLowAce = updateHandValueLowAce(dealerHandValueLowAce, dealerCard);
-        dealerHandValue = updateHandValue(dealerHandValueLowAce, dealerNumAces);
+        // Show play view
+        transition(false, "bet", "play");
+        setTimeout(function () {
+            let dealerCard = getCard();
+            addCard(dealerHand, dealerCard);
+            if (isAce(dealerCard))
+                dealerNumAces++;
+            dealerHandValueLowAce = updateHandValueLowAce(dealerHandValueLowAce, dealerCard);
+            dealerHandValue = updateHandValue(dealerHandValueLowAce, dealerNumAces);
 
-        for (let i = 0; i < 2; i++) {
-            var playerCard = getCard();
-            playerHand[i] = playerCard;
-            if (isAce(playerCard))
-                playerNumAces++;
-            playerHandValueLowAce = updateHandValueLowAce(playerHandValueLowAce, playerCard);
-            playerHandValue = updateHandValue(playerHandValueLowAce, playerNumAces);
+            setTimeout(function () {
+                for (let i = 0; i < 2; i++) {
+                    let playerCard = getCard();
+                    addCard(playerHand, playerCard);
+                    if (isAce(playerCard))
+                        playerNumAces++;
+                    playerHandValueLowAce = updateHandValueLowAce(playerHandValueLowAce, playerCard);
+                    playerHandValue = updateHandValue(playerHandValueLowAce, playerNumAces);
+                }
+                console.log("Dealer: " + dealerHand + " Values: " + dealerHandValue + " " + dealerHandValueLowAce);
+                console.log("Player: " + playerHand + " Values: " + playerHandValue + " " + playerHandValueLowAce);
+                setTimeout(function () {
+                    if (playerHandValue == BLACK_JACK) {
+                        isPlayerDone = true;
+                        updatePlayerMessage("Black Jack!!");
+                    }
+                    showActions();
+                    document.getElementById("hash").innerHTML = hash;
+                    document.getElementById("hashWrapper").style.opacity = 1;
+                }, 500);
+            }, 800);
+        }, 1000);
+    }
+}
+
+function hit() {
+    let card = getCard();
+    addCard(playerHand, card);
+
+    if (isAce(card))
+        playerNumAces++;
+
+    playerHandValueLowAce = updateHandValueLowAce(playerHandValueLowAce, card);
+    playerHandValue = updateHandValue(playerHandValueLowAce, playerNumAces);
+
+    if (playerHandValue > BLACK_JACK) {
+        updatePlayerMessage("Bust!!");
+        endGame();
+    } else if (playerHandValue == BLACK_JACK || playerHandValueLowAce == BLACK_JACK) {
+        updatePlayerMessage("Black Jack!!");
+        endGame();
+    }
+}
+
+function stand() {
+    updatePlayerMessage("");
+    endGame();
+}
+
+function endGame() {
+    document.getElementById("buttonWrapper").style.opacity = document.getElementById("continueWrapper").style.opacity = 0;
+    setTimeout(function () {
+        while (dealerHandValue < 17) {
+            let dealerCard = getCard();
+            addCard(dealerHand, dealerCard);
+            if (isAce(dealerCard))
+                dealerNumAces++;
+            dealerHandValueLowAce = updateHandValueLowAce(dealerHandValueLowAce, dealerCard);
+            dealerHandValue = updateHandValue(dealerHandValueLowAce, dealerNumAces);
         }
-
-        console.log(hash);
 
         console.log("Dealer: " + dealerHand + " Values: " + dealerHandValue + " " + dealerHandValueLowAce);
         console.log("Player: " + playerHand + " Values: " + playerHandValue + " " + playerHandValueLowAce);
+        //setTimeout(function () {}, 1500);
 
-        //isPlayerDone = false;
-        if (playerHandValue == BLACK_JACK) {
-            isPlayerDone = true;
-            console.log("BLACK JACK!!");
+        setTimeout(function () {
+            if (playerHandValue > BLACK_JACK)
+                updatePlayerMessage("House Wins!!");
+            else if ((playerHandValue > dealerHandValue && playerHandValue <= BLACK_JACK) || (dealerHandValue > BLACK_JACK && playerHandValue <= BLACK_JACK)) {
+                playerBalance += currentBet * 2;
+                updatePlayerMessage("You Win!!");
+            } else if (playerHandValue == dealerHandValue && playerHandValue <= BLACK_JACK) {
+                playerBalance += currentBet;
+                updatePlayerMessage("Push! Your bet has been returned.");
+            } else {
+                updatePlayerMessage("Dealer Wins!!");
+            }
+            updateBalance();
+            document.getElementById("buttonWrapper").style.display = document.getElementById("continueWrapper").style.display = "none";
+            document.getElementById("endWrapper").style.display = "flex";
+            setTimeout(function () {
+                document.getElementById("endWrapper").style.opacity = 1;
+            }, 200);
+        }, 1000);
+    }, 1000);
+}
+
+function goToEnd() {
+    console.log(playerBalance);
+    if (playerBalance >= MIN_BET) {
+        document.getElementById("endMessage").innerText = "Continue playing?";
+        var child = document.createElement('a');
+        child.classList = "button";
+        child.onclick = continueGame;
+        child.innerText = "Yes";
+        var parent = document.getElementById("end");
+        parent.insertChildAtIndex(child, 1);
+    }
+    transition(false, "play", "end");
+}
+
+function addCard(hand, card) {
+    let elem;
+    if (hand == dealerHand) {
+        elem = document.getElementsByClassName("cards")[0];
+    } else {
+        elem = document.getElementsByClassName("cards")[1];
+    }
+
+    hand.push(card);
+    var e = document.createElement('img');
+    e.src = "assets/images/cards/" + card + ".png";
+    elem.appendChild(e);
+    window.getComputedStyle(e).opacity;
+    e.className += 'shown';
+    setTimeout(function () {
+        if (elem.childElementCount > 3) {
+            for (let i = 0; i < elem.childElementCount; i++)
+                elem.getElementsByTagName('img')[i].style.height = "20vh";
         }
+    }, 1000);
+}
 
-
-        // Show play view
-        updateBalance();
-        updateDisplayedCards();
-        fixCardOverflow();
-        transition(false, "bet", "play");
+function showActions() {
+    if (isPlayerDone) {
+        document.getElementById("continueWrapper").style.display = "flex";
+        setTimeout(function () {
+            document.getElementById("continueWrapper").style.opacity = 1;
+        }, 200);
+    } else {
+        document.getElementById("buttonWrapper").style.display = "flex";
+        setTimeout(function () {
+            document.getElementById("buttonWrapper").style.opacity = 1;
+        }, 200);
     }
 }
 
-function updateDisplayedCards() {
-    // Clear existing cards
-    for (let i = 0; i < 2; i++)
-        document.getElementsByClassName("cards")[i].innerHTML = "";
-    // Update dealer's cards
-    for (let i = 0; i < dealerHand.length; i++)
-        document.getElementsByClassName("cards")[0].innerHTML += "<img src=\"assets/images/cards/" + dealerHand[i] + ".png\" />";
-    // Update player's cards
-    for (let i = 0; i < playerHand.length; i++)
-        document.getElementsByClassName("cards")[1].innerHTML += "<img src=\"assets/images/cards/" + playerHand[i] + ".png\" />";
-}
-
-function fixCardOverflow() {
-    if (document.getElementById("playerCards").childElementCount > 6) {
-        for (let i = 0; i < document.getElementById("playerCards").childElementCount; i++)
-            document.getElementById("playerCards").getElementsByTagName('img')[i].style.height = "20vh";
-    }
+function updatePlayerMessage(message) {
+    document.getElementById("resultMessage").innerHTML = message;
 }
 
 function transition(useNav, firstSection, secondSection) {
+    updateBalance();
     document.getElementById(firstSection).style.opacity = '0';
     setTimeout(function () {
         document.getElementById(firstSection).style.display = "none";
@@ -101,7 +215,7 @@ function transition(useNav, firstSection, secondSection) {
         document.getElementById(secondSection).style.display = "flex";
         setTimeout(function () {
             if (useNav)
-                document.getElementById("nav").style.opacity = '1'
+                document.getElementById("nav").style.opacity = '1';
             document.getElementById(secondSection).style.opacity = '1';
         }, 100);
     }, 500);
@@ -109,18 +223,16 @@ function transition(useNav, firstSection, secondSection) {
 
 function updateBet() {
     currentBet = parseInt(document.getElementById("betInput").value);
-    //console.log(currentBet); // debug
-
     // Check for errors
     let error = document.getElementById("betInputError");
     if (!currentBet) {
-        error.innerHTML = "Bet is not a valid number!";
+        error.innerHTML = "Please insert a valid number!";
         error.style.display = "block";
     } else if (currentBet < MIN_BET) {
-        error.innerHTML = "Bet is less than minimum!";
+        error.innerHTML = "Unable to bet less than the minimum!";
         error.style.display = "block";
     } else if (currentBet > playerBalance) {
-        error.innerHTML = "Bet is more than your available balance!";
+        error.innerHTML = "Unable to bet more than your available balance!";
         error.style.display = "block";
     } else {
         isBetValid = true;
@@ -134,6 +246,7 @@ function updateBalance() {
 }
 
 function resetValues() {
+    cardStack = [];
     hash = "";
     currentBet = null;
     isBetValid = false;
@@ -146,6 +259,14 @@ function resetValues() {
     dealerHand = [];
     dealerHandValueLowAce = 0;
     dealerHandValue = 0;
+
+    // Clear existing cards
+    for (let i = 0; i < 2; i++)
+        document.getElementsByClassName("cards")[i].innerHTML = "";
+    document.getElementById("endWrapper").style.opacity = 1;
+    document.getElementById("endWrapper").style.display = "none";
+    document.getElementById("resultMessage").innerText = "";
+    document.getElementById("end").innerHTML = "<h1 id=\"endMessage\" class=\"special\">You Lost!</h1> <a class = \"button\" onclick=\"restart();\">Restart</a>"
 }
 
 function enterDetector(e) {
@@ -183,7 +304,13 @@ function updateHash(random) {
 }
 
 function getCard() {
-    return getFace() + getSuit();
+    let newCard = getFace() + getSuit();
+    if (cardStack.includes(newCard)) {
+        while (cardStack.includes(newCard))
+            newCard = getFace() + getSuit();
+    }
+    cardStack.push(newCard);
+    return newCard;
 }
 
 function getFace() {
@@ -228,6 +355,15 @@ function updateHandValue(handValueLowAce, numAces) {
 
 function updateHandValueLowAce(handValue, card) {
     return handValue + getCardValue(card);
+}
+
+Element.prototype.insertChildAtIndex = function (child, index) {
+    if (!index) index = 0
+    if (index >= this.children.length) {
+        this.appendChild(child)
+    } else {
+        this.insertBefore(child, this.children[index])
+    }
 }
 
 /*
