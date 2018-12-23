0be5cdf918c9ae4e34541f0dfa1b86117cd57e4f
KTregubov <31662934+KTregubov@users.noreply.github.com>
2018-12-23 01:05:01 -0500

    Continued Horse Racing work

--- Horse Racing/assets/js/script.js
+++ Horse Racing/assets/js/script.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-    $('.preloader-wrapper').delay(500).fadeOut();
-    $('#welcome').delay(1000).addClass("flex");
+    $('.preloader-wrapper').fadeOut();
+    $('#welcome').delay(10000).removeClass("hidden");
 });
