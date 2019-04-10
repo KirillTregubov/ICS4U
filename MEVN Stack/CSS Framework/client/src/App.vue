970fc0feef66ba5ccb4e138c2534284a59ea8786
KTregubov <31662934+KTregubov@users.noreply.github.com>
2019-04-10 11:03:07 -0400

    Started working on the CSS for the ISP

---
+++ MEVN Stack/CSS Framework/client/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <!-- <OldNav /> -->
+    <Nav />
+
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <!-- <router-view/> -->
+  </div>
+</template>
+
+<script>
+// import OldNav from '@/components/OldNav.vue'
+
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+ul {
+  list-style: none;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ MEVN Stack/CSS Framework/client/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  assetsDir: undefined,
+  publicPath: undefined,
+  outputDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
